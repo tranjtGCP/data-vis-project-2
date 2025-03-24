c8df0422658cd9685eb6aa0b5bc32501905d365a
natehoff3 <icnate02@gmail.com>
2025-03-24 18:26:59 -0400

    Manual Time Zones
    Increased the number of manual time zones to use for converting the given global time to the local time.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 // Array to hold information about the different time zones. Used to convert given global time to local time
 const manualZones = [
   // North America
-  { name: "Pacific Time", latMin: 30, latMax: 50, lonMin: -130, lonMax: -115, offset: -8, label: "PST" },
-  { name: "Mountain Time", latMin: 30, latMax: 50, lonMin: -115, lonMax: -100, offset: -7, label: "MST" },
-  { name: "Central Time", latMin: 30, latMax: 50, lonMin: -100, lonMax: -85, offset: -6, label: "CST" },
-  { name: "Eastern Time", latMin: 30, latMax: 50, lonMin: -85, lonMax: -65, offset: -5, label: "EST" },
+  { name: "Pacific Time (US)", latMin: 32, latMax: 49, lonMin: -125, lonMax: -116, offset: -8, label: "PST" },
+  { name: "Mountain Time (US)", latMin: 31, latMax: 49, lonMin: -116, lonMax: -105, offset: -7, label: "MST" },
+  { name: "Central Time (US)", latMin: 25, latMax: 49, lonMin: -105, lonMax: -90, offset: -6, label: "CST" },
+  { name: "Eastern Time (US)", latMin: 25, latMax: 49, lonMin: -90, lonMax: -67, offset: -5, label: "EST" },
+  { name: "Atlantic Time (Canada)", latMin: 45, latMax: 60, lonMin: -67, lonMax: -52, offset: -4, label: "AST" },
   { name: "Newfoundland Time", latMin: 46, latMax: 53, lonMin: -60, lonMax: -52, offset: -3.5, label: "NST" },
+  { name: "Alaska", latMin: 55, latMax: 72, lonMin: -170, lonMax: -130, offset: -9, label: "AKST" },
+  { name: "Hawaii", latMin: 18, latMax: 22, lonMin: -161, lonMax: -154, offset: -10, label: "HST" },
+  { name: "Greenland", latMin: 59, latMax: 84, lonMin: -72, lonMax: -20, offset: -3, label: "WGT" },
 
-  // Latin America
+  // Caribbean & Latin America
+  { name: "Cuba", latMin: 20, latMax: 24, lonMin: -85, lonMax: -74, offset: -5, label: "CST" },
+  { name: "Puerto Rico", latMin: 17, latMax: 19, lonMin: -68, lonMax: -65, offset: -4, label: "AST" },
   { name: "Argentina", latMin: -55, latMax: -20, lonMin: -75, lonMax: -53, offset: -3, label: "ART" },
   { name: "Brazil (West)", latMin: -20, latMax: 5, lonMin: -75, lonMax: -50, offset: -4, label: "AMT" },
   { name: "Brazil (East)", latMin: -30, latMax: 5, lonMin: -50, lonMax: -35, offset: -3, label: "BRT" },
@@ -19,13 +25,19 @@ const manualZones = [
   { name: "Eastern Europe", latMin: 45, latMax: 60, lonMin: 25, lonMax: 40, offset: 3, label: "EET" },
 
   // Africa
+  { name: "Morocco", latMin: 27, latMax: 36, lonMin: -13, lonMax: -1, offset: 1, label: "WET" },
   { name: "West Africa", latMin: 5, latMax: 20, lonMin: -15, lonMax: 10, offset: 1, label: "WAT" },
   { name: "Central Africa", latMin: -5, latMax: 15, lonMin: 10, lonMax: 30, offset: 2, label: "CAT" },
   { name: "East Africa", latMin: -10, latMax: 15, lonMin: 30, lonMax: 45, offset: 3, label: "EAT" },
+  { name: "South Africa", latMin: -35, latMax: -22, lonMin: 16, lonMax: 33, offset: 2, label: "SAST" },
+  { name: "Egypt", latMin: 22, latMax: 32, lonMin: 25, lonMax: 36, offset: 2, label: "EET" },
 
   // Middle East
   { name: "Turkey", latMin: 36, latMax: 42, lonMin: 26, lonMax: 45, offset: 3, label: "TRT" },
   { name: "Iran", latMin: 25, latMax: 40, lonMin: 44, lonMax: 63, offset: 3.5, label: "IRST" },
+  { name: "Iraq", latMin: 29, latMax: 38, lonMin: 39, lonMax: 49, offset: 3, label: "AST" },
+  { name: "Israel", latMin: 29, latMax: 34, lonMin: 34, lonMax: 36, offset: 2, label: "IST" },
+  { name: "Jordan", latMin: 29, latMax: 33, lonMin: 35, lonMax: 39, offset: 2, label: "EET" },
   { name: "Arabian Peninsula", latMin: 12, latMax: 32, lonMin: 45, lonMax: 60, offset: 4, label: "AST" },
 
   // South Asia
@@ -35,35 +47,33 @@ const manualZones = [
   { name: "Bangladesh", latMin: 20, latMax: 27, lonMin: 88, lonMax: 93, offset: 6, label: "BST" },
   { name: "Myanmar", latMin: 9, latMax: 28, lonMin: 93, lonMax: 101, offset: 6.5, label: "MMT" },
 
-  // East Asia
+  // East & Southeast Asia
+  { name: "Thailand", latMin: 5, latMax: 21, lonMin: 97, lonMax: 106, offset: 7, label: "THA" },
+  { name: "Malaysia", latMin: 1, latMax: 7, lonMin: 100, lonMax: 120, offset: 8, label: "MYT" },
+  { name: "Philippines", latMin: 5, latMax: 21, lonMin: 116, lonMax: 127, offset: 8, label: "PHT" },
   { name: "China", latMin: 20, latMax: 45, lonMin: 100, lonMax: 125, offset: 8, label: "CST" },
   { name: "Korea", latMin: 33, latMax: 43, lonMin: 125, lonMax: 130, offset: 9, label: "KST" },
   { name: "Japan", latMin: 30, latMax: 45, lonMin: 130, lonMax: 146, offset: 9, label: "JST" },
 
-  // Southeast Asia
-  { name: "Thailand", latMin: 5, latMax: 21, lonMin: 97, lonMax: 106, offset: 7, label: "THA" },
-  { name: "Malaysia", latMin: 1, latMax: 7, lonMin: 100, lonMax: 120, offset: 8, label: "MYT" },
-  { name: "Philippines", latMin: 5, latMax: 21, lonMin: 116, lonMax: 127, offset: 8, label: "PHT" },
+  // Indonesia
   { name: "Indonesia (West)", latMin: -7, latMax: 6, lonMin: 95, lonMax: 105, offset: 7, label: "WIB" },
   { name: "Indonesia (Central)", latMin: -7, latMax: 6, lonMin: 105, lonMax: 125, offset: 8, label: "WITA" },
   { name: "Indonesia (East)", latMin: -7, latMax: 6, lonMin: 125, lonMax: 141, offset: 9, label: "WIT" },
 
-  // Australia
+  // Australia & Oceania
   { name: "Western Australia", latMin: -35, latMax: -10, lonMin: 110, lonMax: 130, offset: 8, label: "AWST" },
   { name: "Central Australia", latMin: -35, latMax: -22, lonMin: 130, lonMax: 140, offset: 9.5, label: "ACST" },
   { name: "Eastern Australia", latMin: -38, latMax: -10, lonMin: 140, lonMax: 155, offset: 10, label: "AEST" },
-
-  // New Zealand
   { name: "New Zealand", latMin: -47, latMax: -34, lonMin: 165, lonMax: 180, offset: 12, label: "NZST" },
+  { name: "Fiji", latMin: -19, latMax: -16, lonMin: 176, lonMax: 180, offset: 12, label: "FJT" },
+  { name: "Samoa", latMin: -14, latMax: -13, lonMin: -172, lonMax: -171, offset: 13, label: "WST" },
+  { name: "Tonga", latMin: -22, latMax: -15, lonMin: -176, lonMax: -173, offset: 13, label: "TOT" },
 
-  // Russia (general zones)
-  { name: "Russia West", latMin: 50, latMax: 65, lonMin: 30, lonMax: 60, offset: 3, label: "MSK" },
-  { name: "Russia Central", latMin: 50, latMax: 65, lonMin: 60, lonMax: 90, offset: 6, label: "OMST" },
-  { name: "Russia East", latMin: 50, latMax: 65, lonMin: 90, lonMax: 130, offset: 9, label: "YAKT" },
-
-  // Alaska & Hawaii
-  { name: "Alaska", latMin: 55, latMax: 72, lonMin: -170, lonMax: -130, offset: -9, label: "AKST" },
-  { name: "Hawaii", latMin: 18, latMax: 22, lonMin: -161, lonMax: -154, offset: -10, label: "HST" }
+  // Russia (subdivided)
+  { name: "Kaliningrad", latMin: 53, latMax: 56, lonMin: 19, lonMax: 23, offset: 2, label: "USZ1" },
+  { name: "Moscow", latMin: 54, latMax: 58, lonMin: 35, lonMax: 40, offset: 3, label: "MSK" },
+  { name: "Novosibirsk", latMin: 52, latMax: 56, lonMin: 75, lonMax: 85, offset: 7, label: "NOVT" },
+  { name: "Vladivostok", latMin: 43, latMax: 47, lonMin: 130, lonMax: 135, offset: 10, label: "VLAT" }
 ];
 
 function getTimezoneOffset(lat, lon) {
