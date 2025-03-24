1b3e483c361af49b5a33509c1592b4ab6c0f7da7
natehoff3 <icnate02@gmail.com>
2025-03-24 17:36:49 -0400

    Filter data
    Added the filtering of data to check for any empty or invalid values.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ d3.csv('data/2024-2025.csv')  //**** TO DO  switch this to loading the quakes 'd
 .then(data => {
     console.log("number of items: " + data.length);
 
+    // Filter data for any empty or invalid values
+    data = data.filter(d =>
+      d.latitude !== "" && d.longitude !== "" && d.depth !== "" &&
+      d.mag !== "" && d.time !== "" && d.place !== "" && d.dmin !== "" &&
+      !isNaN(+d.latitude) && !isNaN(+d.longitude) &&
+      !isNaN(+d.depth) && !isNaN(+d.mag) && !isNaN(+d.dmin)
+    );
+
     data.forEach(d => {  //convert from string to number
       d.latitude = +d.latitude; 
       d.longitude = +d.longitude;  
